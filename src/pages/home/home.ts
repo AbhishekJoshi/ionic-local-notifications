8a72f3902ba0abd5494cf637152eaea9a7bd326e
Abhishek Joshi <abhishek.joshi20@gmail.com>
2018-06-27 00:55:22 -0400

    Test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,19 +13,82 @@ export class HomePage {
 
   }
 
-  public doRepeatSchedule() {
+  public createSingleNotification(){
+    cordova.plugins.notification.local.schedule({
+      title: 'A single notification',
+      text: 'Thats pretty easy...',
+      foreground: true,
+    });
+  }
 
+  public doRepeatSchedule() {
     cordova.plugins.notification.local.schedule({
       title: 'I see this...',
-      text: '5 times ',
-      trigger: { every: 'minute', count: 5 },
+      text: 'every minute ',
+      trigger: { every: 'minute' },
       foreground: true,
     }).then(res => {
       console.log('Scheduled! ', res);
     }).catch(err => {
       console.error('Error scheduling', err);
     });
+  }
 
+
+
+  public createInTwoMinutes() {
+    cordova.plugins.notification.local.schedule({
+      title: 'Three minute meeting',
+      trigger: { in: 3, unit: 'minute' }
+    });
+  }
+
+  public clearSchedule300(){
+    cordova.plugins.notification.local.clear(300);
+  }
+
+  public createID400(){
+    cordova.plugins.notification.local.schedule({
+      id: 400,
+      title: 'ID 400',
+      text: 'ID 400',
+      trigger: { in: 3, unit: 'minute' }
+    });
+  }
+
+  public createID300(){
+    cordova.plugins.notification.local.schedule({
+      id: 300,
+      title: 'ID 300',
+      text: 'ID 300',
+      trigger: { in: 3, unit: 'minute' }
+    });
+  }
+
+  public getSchedule(){
+
+    console.log('----------');
+    console.log('----------');
+    console.log('----------');
+    cordova.plugins.notification.local.getScheduledIds(function (ids) {
+      console.log('getScheduledIds ', ids);
+    });
+    cordova.plugins.notification.local.getScheduled(function (ids) {
+      console.log('getScheduled ', ids);
+    });
+
+    cordova.plugins.notification.local.getTriggered(function (ids) {
+      console.log('getTriggered ', ids);
+    });
+
+
+    cordova.plugins.notification.local.getTriggeredIds(function (ids) {
+      console.log('getTriggeredIds ', ids);
+    });
+
+    cordova.plugins.notification.local.getAll(function (nots) {
+      console.log('getAll ', nots);
+    });
   }
 }
 
